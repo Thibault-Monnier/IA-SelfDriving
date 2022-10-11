3f4381f9eebf9f38a0d7e1db88da794d151e90f4
Thibault Monnier <thibaultmonni@gmail.com>
2022-10-11 19:20:42 +0200

    Car turns when side keys pressed

--- car.js
+++ car.js
@@ -9,6 +9,7 @@ class Car {
     this.acceleration = 0.2
     this.maxSpeed = 3
     this.friction = 0.05
+    this.angle = 0
 
     this.controls = new Controls()
   }
@@ -21,10 +22,10 @@ class Car {
       this.speed -= this.acceleration
     }
     if (this.controls.left) {
-      this.x -= 2
+      this.angle += 0.03
     }
-    if(this.controls.right) {
-      this.x += 2
+    if (this.controls.right) {
+      this.angle -= 0.03
     }
 
     if (this.speed > this.maxSpeed) {
@@ -48,13 +49,14 @@ class Car {
   }
 
   draw(ctx) {
+    ctx.save()
+    ctx.translate(this.x, this.y)
+    ctx.rotate(-this.angle)
+
     ctx.beginPath()
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    )
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height)
     ctx.fill()
+
+    ctx.restore()
   }
 }
--- main.js
+++ main.js
@@ -10,9 +10,8 @@ animate()
 
 function animate() {
   car.update()
-  
+
   canvas.height = window.innerHeight
   car.draw(ctx)
   requestAnimationFrame(animate)
 }
-
