6f42fc38a00d69c9e613725705842acf78a83ff3
Thibault Monnier <thibaultmonni@gmail.com>
2023-05-10 20:57:12 +0200

    Nb cars

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ const networkCtx = networkCanvas.getContext("2d")
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9)
 
-const N = 10000
+const N = 1000
 const cars = generateCars(N)
 let bestCar = cars[0]
 if (localStorage.getItem("bestBrain")) {
   for (let i = 0; i < cars.length; i++) {
     cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"))
     if (i != 0) {
-      NeuralNetwork.mutate(cars[i].brain, 0.1)
+      NeuralNetwork.mutate(cars[i].brain, 0.3)
     }
   }
 }
